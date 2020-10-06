0693ab25bf441ce27f468e6fe6bbb59635ec2880
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-06 16:11:50 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,11 @@
 import os
 import cv2
 import numpy as np
-from torch.utils.data import Dataset
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+import segmentation_models_pytorch as smp
+
+from dataset_utils import get_training_augmentation, get_validation_augmentation, get_preprocessing
 
 class SegDataset(Dataset):
     def __init__(
@@ -23,8 +27,8 @@ class SegDataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        path_to_image = os.join(self.root_dir, self.data['image'][idx])
-        path_to_mask = os.join(self.root_dir, self.data['mask'][idx])
+        path_to_image = os.join(self.root_dir, self.data.image[idx])
+        path_to_mask = os.join(self.root_dir, self.data.mask[idx])
         
         image = cv2.imread(path_to_image)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -41,3 +45,27 @@ class SegDataset(Dataset):
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
         return image, mask
+   
+def get_dataloader(
+    path2csv_dataset,
+    num_cls2name,
+    encoder,
+    encoder_weigchts,
+    root_dir='./data',
+    is_train=True,
+    batch_size=8,
+    shuffle=True,
+    num_workers=12
+):
+    df = pd.read_csv(path2csv_dataset, names=['image', 'mask'])
+    preprocessing_func = smp.encoders.get_preprocessing_fn(encoder, encoder_weigchts)
+    augmentation = get_training_augmentation if is_train else get_validation_augmentation
+    dataset = SegDataset(
+        root_dir=root_dir,
+        df_paths=df,
+        num_cls2name=num_cls2name,
+        augmentation=augmentation(), 
+        preprocessing=get_preprocessing(preprocessing_func),
+    )
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    
