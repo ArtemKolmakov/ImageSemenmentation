eab010d5070c7bf3ff1d09e6a6903bc87e52e878
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-06 15:45:11 +0700

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import segmentation_models_pytorch as smp
 from catalyst.dl.runner.supervised import SupervisedRunner
 from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
 
-from dataset import get_dataset
+from dataset import get_datloader
 
 
 
