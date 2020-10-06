4ab63e84de4bda5196048f44b85ac9e66634a5b9
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-06 15:39:00 +0700

    Create train.py
    add imports

---
+++ train.py
@@ -0,0 +1,10 @@
+import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import segmentation_models_pytorch as smp
+from catalyst.dl.runner.supervised import SupervisedRunner
+from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
+
+from dataset import get_dataset
+
+
+
