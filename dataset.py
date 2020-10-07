f9ded5acaf1006622878121e3331fe5977aaa2ab
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-07 15:34:21 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -10,16 +10,13 @@ from dataset_utils import get_training_augmentation, get_validation_augmentation
 class SegDataset(Dataset):
     def __init__(
         self,
-        root_dir,
         df_paths,
-        num_cls2name,
+        num_cls,
         augmentation=None, 
         preprocessing=None,
     ):
-        self.root_dir = root_dir
         self.data = df_paths
-        self.num_classes = len(num_cls2name)
-        self.cls_encoder = num_cls2name
+        self.num_classes = num_cls
         self.augmentation = augmentation
         self.preprocessing = preprocessing
     
@@ -27,14 +24,12 @@ class SegDataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        path_to_image = os.join(self.root_dir, self.data.image[idx])
-        path_to_mask = os.join(self.root_dir, self.data.mask[idx])
         
-        image = cv2.imread(path_to_image)
+        image = cv2.imread(self.data.images[idx])
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
-        mask = cv2.imread(path_to_mask, 0)
-        masks = [(mask == v) for v in num_cls2name]
+        mask = cv2.imread(self.data.masks[idx], 0)
+        masks = [(mask == v) for v in range(self.num_classes)]
         mask = np.stack(masks, axis=-1).astype('float')
         
         if self.augmentation:
@@ -45,25 +40,27 @@ class SegDataset(Dataset):
             sample = self.preprocessing(image=image, mask=mask)
             image, mask = sample['image'], sample['mask']
         return image, mask
-   
+  
+
+ 
 def get_dataloader(
     path2csv_dataset,
-    num_cls2name,
     encoder,
-    encoder_weigchts,
-    root_dir='./data',
+    encoder_weights,
+    num_cls=12,
     is_train=True,
     batch_size=8,
     shuffle=True,
     num_workers=12
 ):
-    df = pd.read_csv(path2csv_dataset, names=['image', 'mask'])
-    preprocessing_func = smp.encoders.get_preprocessing_fn(encoder, encoder_weigchts)
+    df = pd.read_csv(path2csv_dataset, sep=' ', names=['images', 'masks'])
+    df.images = df.images.map(lambda x: x.replace('/SegNet', 'data'))
+    df.masks = df.masks.map(lambda x: x.replace('/SegNet', 'data'))
+    preprocessing_func = smp.encoders.get_preprocessing_fn(encoder, encoder_weights)
     augmentation = get_training_augmentation if is_train else get_validation_augmentation
     dataset = SegDataset(
-        root_dir=root_dir,
         df_paths=df,
-        num_cls2name=num_cls2name,
+        num_cls=num_cls,
         augmentation=augmentation(), 
         preprocessing=get_preprocessing(preprocessing_func),
     )
