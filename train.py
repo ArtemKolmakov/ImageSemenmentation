a005e2a1f8d9d35ec6ae35c7952622b0a2c6b320
ArtemKolmakov <46863989+ArtemKolmakov@users.noreply.github.com>
2020-10-07 15:37:31 +0700

    Update train.py

--- train.py
+++ train.py
@@ -4,46 +4,42 @@ import segmentation_models_pytorch as smp
 from catalyst.dl.runner.supervised import SupervisedRunner
 from catalyst.dl.callbacks import DiceCallback, EarlyStoppingCallback, InferCallback, CheckpointCallback
 
-from dataset import get_datloader
+from dataset import get_dataloader
 
-root_dir = './data'
-path2train_csv = './data/train.csv'
-path2val_csv = './data/val.csv'
-path2test_csv = './data/test.csv'
+path2train_csv = 'data/CamVid/train.txt'
+path2val_csv = 'data/CamVid/val.txt'
+path2test_csv = 'data/CamVid/test.txt'
 
-num_cls2name = {1: 'car'}
+num_cls = 12
 
 encoder = 'se_resnext50_32x4d'
 encoder_weights = 'imagenet'
 activation = 'sigmoid'
 
-train_dataloader = get_datloader(
-  path2train_csv=path2train_csv,
-  num_cls2name=num_cls2name,
+train_dataloader = get_dataloader(
+  path2csv_dataset=path2train_csv,
+  num_cls=num_cls,
   encoder=encoder,
-  encoder_weigchts=encoder_weigchts,
-  root_dir=root_dir,
+  encoder_weights=encoder_weights,
   is_train=True,
   batch_size=8,
   num_workers=12,
 )
-val_dataloader = (
-  path2val_csv=path2val_csv,
-  num_cls2name=num_cls2name,
+val_dataloader = get_dataloader(
+  path2csv_dataset=path2val_csv,
+  num_cls=num_cls,
   encoder=encoder,
-  encoder_weigchts=encoder_weigchts,
-  root_dir='./data',
+  encoder_weights=encoder_weights,
   is_train=False,
   batch_size=1,
   shuffle=False,
   num_workers=4,
 )
-test_dataloader = (
-  path2val_csv=path2test_csv,
-  num_cls2name=num_cls2name,
+test_dataloader = get_dataloader(
+  path2csv_dataset=path2test_csv,
+  num_cls=num_cls,
   encoder=encoder,
-  encoder_weigchts=encoder_weigchts,
-  root_dir='./data',
+  encoder_weights=encoder_weights,
   is_train=False,
   batch_size=1,
   shuffle=False,
@@ -59,7 +55,7 @@ loaders = {
 model = smp.FPN(
     encoder_name=encoder, 
     encoder_weights=encoder_weights, 
-    classes=len(num_cls2name), 
+    classes=num_cls, 
     activation=activation,
 )
 
@@ -71,7 +67,7 @@ optimizer = torch.optim.Adam([
     {'params': model.encoder.parameters(), 'lr': 1e-3},  
 ])
 scheduler = ReduceLROnPlateau(optimizer, factor=0.15, patience=2)
-criterion = smp.utils.losses.BCEDiceLoss(eps=1.)
+criterion = smp.utils.losses.DiceLoss(eps=1.)
 runner = SupervisedRunner()
 
 runner.train(
@@ -85,5 +81,3 @@ runner.train(
     num_epochs=num_epochs,
     verbose=True
 )
-
-
